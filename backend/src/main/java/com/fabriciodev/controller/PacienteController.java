80a9aab719484cb84b646ff267a60aab579b8b1f
FabricioWashington <fabriciowashington2015@gmail.com>
2025-03-20 22:28:19 -0300

    ajustando cors

--- backend/src/main/java/com/fabriciodev/controller/PacienteController.java
+++ backend/src/main/java/com/fabriciodev/controller/PacienteController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pacientes")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class PacienteController {
 
     @Autowired
